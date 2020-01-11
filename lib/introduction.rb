98f0301cbeeb493fd6ca47ba80c913c7c0eac214
Ramzy Badawy <ramzy.badawy@gmail.com>
2020-01-11 19:31:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,9 @@
-def introduction (name)
-puts "Hi, my name is #{name}."
-end
+def introduction_with_language ("name", "language")
 
-introduction (Ramzy)
+puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end 
 
-
-
-
-
-
+introduction_with_language (Ramzy, Ruby)
 
 
 
